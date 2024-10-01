d58cf0d11294713d454d45d030e0a803fd0aa081
Navaneet R Rao <76439641+navaneet-rao@users.noreply.github.com>
2024-10-01 23:24:39 +0530

    feat: starting of setup file added check requirements and dependencies func

---
+++ setup/setup.sh
@@ -0,0 +1,95 @@
+#!/bin/sh -e
+
+# ## for bash
+# RC='\033[0m'
+# RED='\033[31m'
+# YELLOW='\033[33m'
+# GREEN='\033[32m'
+
+## for zsh
+RC=$'\033[0m'
+RED=$'\033[31m'
+YELLOW=$'\033[33m'
+GREEN=$'\033[32m'
+
+# add variables to top level so can easily be accessed by all functions
+PACKAGER=""
+SUDO_CMD=""
+SUGROUP=""
+GITPATH=""
+
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+checkEnv() {
+    
+    echo "${YELLOW}Checkimg for requirements...${RC}"
+
+    ## Check for requirements.
+    REQUIREMENTS='curl groups sudo'
+    for req in $REQUIREMENTS; do
+        if ! command_exists "$req"; then
+            echo "${RED}To run me, you need: $REQUIREMENTS${RC}"
+            exit 1
+        fi
+    done
+
+    ## Check Package Handler
+    PACKAGEMANAGER='nala apt dnf yum pacman zypper emerge xbps-install nix-env'
+    for pgm in $PACKAGEMANAGER; do
+        if command_exists "$pgm"; then
+            PACKAGER="$pgm"
+            echo "Using $pgm"
+            break
+        fi
+    done
+
+    if [ -z "$PACKAGER" ]; then
+        echo "${RED}Can't find a supported package manager${RC}"
+        exit 1
+    fi
+
+    if command_exists sudo; then
+        SUDO_CMD="sudo"
+    elif command_exists doas && [ -f "/etc/doas.conf" ]; then
+        SUDO_CMD="doas"
+    else
+        SUDO_CMD="su -c"
+    fi
+
+    echo "Using $SUDO_CMD as privilege escalation software"
+
+    ## Check if the current directory is writable.
+    GITPATH=$(dirname "$(realpath "$0")")
+    if [ ! -w "$GITPATH" ]; then
+        echo "${RED}Can't write to $GITPATH${RC}"
+        exit 1
+    fi
+
+    ## Check SuperUser Group
+    SUPERUSERGROUP='wheel sudo root'
+    for sug in $SUPERUSERGROUP; do
+        if groups | grep -q "$sug"; then
+            SUGROUP="$sug"
+            echo "Super user group $SUGROUP"
+            break
+        fi
+    done
+
+    ## Check if member of the sudo group.
+    if ! groups | grep -q "$SUGROUP"; then
+        echo "${RED}You need to be a member of the sudo group to run me!${RC}"
+        exit 1
+    fi
+}
+
+installDepend() {
+
+    echo "${YELLOW}Checking for dependencies...${RC}"
+
+}
+
+checkEnv
+installDepend
+
